d4ee984440f9910ac22bdde166c88c0b6b654fdd
yeliex <yeliex@yeliex.com>
2025-03-07 15:35:18 +0800

    chore: fix release tag

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 pnpm changeset tag
 
-# 如果git分支是master 使用latest标签 否则使用分支名
-npm_tag='latest';
-# 获取分支名
+npm_tag='';
 branch_name=$(git symbolic-ref --short -q HEAD);
 if [ $branch_name != 'master' ]; then
-  npm_tag=$branch_name;
+    npm_tag="latest"
+else
+    if [[ $branch_name =~ ^v[0-9]+$ ]]; then
+        npm_tag="latest-${branch_name}"
+    else
+        npm_tag=$branch_name
+    fi
 fi
 
 pnpm publish --recursive --publish-branch $branch_name --tag $npm_tag

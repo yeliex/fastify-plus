902b016742c80470b329221205ee4723c5feebdd
yeliex <yeliex@yeliex.com>
2025-03-07 11:54:05 +0800

    chore: update release workflow

---
+++ scripts/release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pnpm changeset tag
+
+# 如果git分支是master 使用latest标签 否则使用分支名
+npm_tag='latest';
+# 获取分支名
+branch_name=$(git symbolic-ref --short -q HEAD);
+if [ $branch_name != 'master' ]; then
+  npm_tag=$branch_name;
+fi
+
+pnpm publish --recursive --publish-branch $branch_name --tag $npm_tag

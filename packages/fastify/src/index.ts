62b711c3378e9b3c94219e7d0f9e0f5d841920d8
yeliex <yeliex@yeliex.com>
2025-07-30 15:31:33 +0800

    chore: update package

--- packages/fastify/src/index.ts
+++ packages/fastify/src/index.ts
@@ -7,3 +7,7 @@ export { default as createServer, DEFAULT_OPTIONS } from './server.js';
 export * from './libs/util.js';
 
 export { default as RouterLoaderPlugin } from './plugins/router/index.js';
+
+// to keep reference in bundled dts
+// export { default as fastifyAccepts } from '@fastify/accepts';
+// export { default as fastifyCookie } from '@fastify/cookie';
